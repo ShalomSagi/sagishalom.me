5bec39314cc23ce13d00d924aabe92eee341c349
Sagi <sagi@Sagi-2.local>
2020-07-05 15:36:44 +0300

    Added CV Elements

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from "react";
 import HeroSection from "./Components/HeroSection/heroSection";
 import AboutMe from "./Components/AboutMe/aboutMe";
 import Skills from "./Components/Skills/skills";
+import EmploymentHistory from "./Components/EmploymentHistory/employmentHistory";
+import Education from "./Components/Education/education";
+import PersonalProject from "./Components/PersonalProject/personalProject";
+import Freelance from "./Components/Freelance/freelance";
+import Languages from "./Components/Languages/languages";
 
 import "./App.scss";
 
@@ -11,9 +16,17 @@ function App() {
       {/* <Header /> */}
       <HeroSection />
       <AboutMe />
+      <Languages />
       <div className="double">
-        <div></div>
-        <Skills />
+        <div className="row">
+          <EmploymentHistory />
+          <Freelance />
+        </div>
+        <div className="row">
+          <Skills />
+          <Education />
+          <PersonalProject />
+        </div>
       </div>
     </div>
   );
