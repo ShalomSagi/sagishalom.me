ce71c5798abf8d6a0b99cd72ea4c0711565632a4
Sagi <sagi@Sagi-2.local>
2020-07-05 15:37:27 +0300

    Added a "Download CV" button

--- src/Components/AboutMe/aboutMe.js
+++ src/Components/AboutMe/aboutMe.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./aboutMe.module.scss";
 import CodeEffect from "../../Utils/CodeEffect/codeEffect";
 
 const AboutMe = () => {
   return (
     <div className={classes["about-me"]}>
-      <div className={classes.title}>Who Am I?</div>
+      <CodeEffect>
+        <div className={classes.title}>Who Am I?</div>
+      </CodeEffect>
       <div className={classes.content}>
-        <CodeEffect doneHandler={() => {}}>
+        <CodeEffect>
           <p>
             <b>Passionate front-end developer</b> with back-end experience and a
             strong sense for aesthetics.
@@ -21,6 +23,15 @@ const AboutMe = () => {
             Looking for a position where my current skills can bring value and
             where I can gain new skills and experience.
           </p>
+          <div className={classes.center}>
+            <a
+              href="/download/CV-Sagi-Shalom.pdf"
+              className={classes.download}
+              download="CV - Sagi Shalom.pdf"
+            >
+              Download My CV
+            </a>
+          </div>
         </CodeEffect>
       </div>
     </div>
