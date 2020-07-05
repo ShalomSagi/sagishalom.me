0e631c04475c615ded97d3a65c3b10b56439e0c2
Sagi <sagi@Sagi-2.local>
2020-07-05 15:41:24 +0300

    Created InfoBox component

---
+++ src/Utils/InfoBox/infoBox.js
@@ -0,0 +1,35 @@
+import React from "react";
+import classes from "./infoBox.module.scss";
+import CodeEffect from "../CodeEffect/codeEffect";
+
+const InfoBox = (props) => {
+  return (
+    <CodeEffect>
+      <div className={classes["history-item"]}>
+        <div className={classes["title-area"]}>
+          <div className={classes.time}>{props.time}</div>
+          <div className={classes["job-title"]}>
+            <div className={classes.title}>{props.title}</div>
+            <div className={classes.at}>{props.at}</div>
+          </div>
+        </div>
+        <div className={props.achievements ? classes.achievements : ""}>
+          {props.achievements
+            ? props.achievements.map((achievement, index) => {
+                return (
+                  <p className={classes.achievement} key={index}>
+                    {achievement}
+                  </p>
+                );
+              })
+            : null}
+          {props.citation ? (
+            <p className={classes.citation}>{props.citation}</p>
+          ) : null}
+        </div>
+      </div>
+    </CodeEffect>
+  );
+};
+
+export default InfoBox;
