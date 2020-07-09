5cf08b29ee12883a5828aac944ef621e370ff38a
Sagi <sagi@Sagi-2.local>
2020-07-09 14:18:22 +0300

    Adding animations to the Languages component

--- src/Components/Languages/languages.js
+++ src/Components/Languages/languages.js
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./languages.module.scss";
+import "./languages.animations.scss";
 
-const Languages = () => {
+const Languages = (props) => {
   return (
     <div className={classes.wrapper}>
       <div className={classes.list}>
         <div>
-          <div className={classes.language}>Hebrew</div>
-          <div className={classes.level}>Native</div>
+          <div className={classes.language}>
+            <div className={props.isVisible ? "slide-up" : "zero-opacity"}>
+              Hebrew
+            </div>
+          </div>
+          <div className={classes.level}>
+            <div className={props.isVisible ? "slide-down" : "zero-opacity"}>
+              Native
+            </div>
+          </div>
         </div>
         <div>
-          <div className={classes.language}>English</div>
-          <div className={classes.level}>Proficient</div>
+          <div className={classes.language}>
+            <div
+              className={props.isVisible ? "slide-up" : "zero-opacity"}
+              style={{ animationDelay: "0.4s" }}
+            >
+              English
+            </div>
+          </div>
+          <div className={classes.level}>
+            <div
+              className={props.isVisible ? "slide-down" : "zero-opacity"}
+              style={{ animationDelay: "0.4s" }}
+            >
+              Proficient
+            </div>
+          </div>
         </div>
       </div>
     </div>
